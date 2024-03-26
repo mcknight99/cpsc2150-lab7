1556f9b0a86214ed8738397ab0ed3746943bfc3a
Sam Pupke <sampupke@gmail.com>
2024-03-26 17:17:02 -0400

    Update IShuffleList.java

--- cpsc2150/listDec/IShuffleList.java
+++ cpsc2150/listDec/IShuffleList.java
@@ -3,4 +3,17 @@ package cpsc2150.listDec.cpsc2150.listDec;
 import java.util.List;
 
 public interface IShuffleList<T> extends List<T> {
+    /**
+     *Randomly picks two positions in the list and swaps them
+     * Repeats this swaps times
+     * @param swaps
+     */
+    default void shuffle(int swaps) {}
+
+    /**
+     * Exchanges the values at positions i and j in the list
+     * @param i
+     * @param j
+     */
+    default void swap(int i, int j) {}
 }
